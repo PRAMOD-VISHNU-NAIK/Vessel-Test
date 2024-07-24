b3024cee5f742aadf012a47a7907acaf1b490b33
Pramod Naik <pammunaik99.51solution@gmail.com>
2024-07-24 16:57:15 +0530

    Modified the Build Files

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 setup(
     name='addition',
     version='0.1.0',
-    packages=find_packages(where='app'),
-    package_dir={'': 'app'},
-    py_modules=['addition'],
-    entry_points={
+    packages=find_packages(where='app'),    #This will searches for packages within the app directory.
+    package_dir={'': 'app'},            # indicates that the root of the package hierarchy is in the app directory.
+    py_modules=['addition'],            # THis specifies that the addition module should be included in the distribution. This assumes that there is an addition.py file directly in the app directory.
+    entry_points={                      #The entry_points argument specifies that a console script named addition should be created, which will execute the add function from the addition module when called.
         'console_scripts': [
             'addition = addition:add',
         ],
