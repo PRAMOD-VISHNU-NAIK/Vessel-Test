140855fa26f84da99b2ca4bfa57acda2ceac27f3
Pramod Naik <pammunaik99.51solution@gmail.com>
2024-07-24 17:42:26 +0530

    Included Dynamic Versioning

--- ReadMe.md
+++ ReadMe.md
@@ -9,3 +9,13 @@ from addition import add
 
 result = add(2, 3)
 print(result)  # Output: 5
+
+
+
+
+## MANIFEST.in
+Error: version.py file is not being found during the build process because it's not being included in the source distribution (sdist). To fix this, we need to ensure that version.py is included in the package.
+
+To include version.py in the distribution, you can use a MANIFEST.in file and add the below code
+
+    include version.py
--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 # setup.py
-
+import os
 from setuptools import setup, find_packages
+
+def read_version():
+    version = {}
+    version_file_path = os.path.join(os.path.dirname(__file__), 'version.py')
+    if os.path.exists(version_file_path):
+        with open(version_file_path) as fp:
+            exec(fp.read(), version)
+        return version['__version__']
+    else:
+        raise FileNotFoundError(f"Version file not found: {version_file_path}")
 
 setup(
     name='addition',
-    version='0.1.0',
+    version=read_version(),             # Read version dynamically from version.py
     packages=find_packages(where='app'),    #This will searches for packages within the app directory.
     package_dir={'': 'app'},            # indicates that the root of the package hierarchy is in the app directory.
     py_modules=['addition'],            # THis specifies that the addition module should be included in the distribution. This assumes that there is an addition.py file directly in the app directory.
